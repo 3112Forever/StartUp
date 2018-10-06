c34be52da26d99c136a17424b97d556e8c38bad4
3112Forever <jknoproblem3112@gail.com>
2018-10-06 11:16:07 +0530

    create statefulwidget and used random word

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final wordPair = WordPair.random();
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Learning Demo',
+      title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Learn'),
+          title: Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body:  Center(
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
